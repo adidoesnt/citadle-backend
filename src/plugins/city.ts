af99d8ea81633d6f25173cce58ebde822bf50c00
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-02-22 19:25:24 +0800

    secure important routes with api key

--- src/plugins/city.ts
+++ src/plugins/city.ts
@@ -1,15 +1,28 @@
 import { getCurrentCityFromRedis } from "components";
 import { Elysia } from "elysia";
 
+const { API_KEY: apiKey } = process.env;
+
 export const city = new Elysia({
   prefix: "city",
-}).get("/", async ({ set }) => {
-  try {
-    const city = await getCurrentCityFromRedis();
-    set.status = 200;
-    return city;
-  } catch (error) {
-    set.status = 500;
-    return error;
+}).get(
+  "/",
+  async ({ set }) => {
+    try {
+      const city = await getCurrentCityFromRedis();
+      set.status = 200;
+      return city;
+    } catch (error) {
+      set.status = 500;
+      return error;
+    }
+  },
+  {
+    beforeHandle: ({ headers, set }) => {
+      if (headers["x-api-key"] !== apiKey) {
+        set.status = 401;
+        return "Unauthorized";
+      }
+    },
   }
-});
+);
