b59e9b3103e9d214d99b117500890d251cd906a0
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-02-23 16:24:38 +0800

    update cors config

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -1,7 +1,20 @@
 import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
 
-const { FRONTEND_URL, NODE_ENV = "DEV" } = process.env;
-const corsConfig = NODE_ENV === "DEV" ? undefined : { origin: FRONTEND_URL };
+const { FRONTEND_URL = "", NODE_ENV = "DEV" } = process.env;
+const corsConfig =
+  NODE_ENV === "DEV"
+    ? undefined
+    : {
+        origin(request: Request) {
+          if (corsConfig) {
+            const { origin } = request.headers as Headers & { origin: any };
+            if (origin && FRONTEND_URL.includes(origin)) {
+              return origin;
+            }
+          }
+          return false;
+        },
+      };
 
 export const corsPlugin = (app: Elysia) => app.use(cors(corsConfig));
