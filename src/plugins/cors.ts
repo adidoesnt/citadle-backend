382c28b2d196ea3e6566204cab6db311ef0933d6
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-02-23 16:28:01 +0800

    remove cors

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -1,20 +1,4 @@
 import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
 
-const { FRONTEND_URL = "", NODE_ENV = "DEV" } = process.env;
-const corsConfig =
-  NODE_ENV === "DEV"
-    ? undefined
-    : {
-        origin(request: Request) {
-          if (corsConfig) {
-            const { origin } = request.headers as Headers & { origin: any };
-            if (origin && FRONTEND_URL.includes(origin)) {
-              return origin;
-            }
-          }
-          return false;
-        },
-      };
-
-export const corsPlugin = (app: Elysia) => app.use(cors(corsConfig));
+export const corsPlugin = (app: Elysia) => app.use(cors());
